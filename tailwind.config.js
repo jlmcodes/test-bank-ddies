ea010d5198955dc6f6d5f83f817e237f480cf2e5
Xeia <vpacads.nudjpia2526@gmail.com>
2026-04-18 03:23:33 +0800

    Fixed dark mode text clash

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class', // <--- This tells it to only use our app's toggle button!
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
